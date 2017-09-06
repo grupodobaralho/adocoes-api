b40a0fd93ca2a3ae7309bc9c60cab1730c3e3884
Unknown <gregory_lagranha@hotmail.com>
2017-09-06 18:40:38 -0300

    Correção da validação do campo data de nascimento e  campo etnia
    Correção do problema da validação da dataNascimento.
    Alteração do campo refEtnia da classe menor para apenas Etnia com a utilização de "enum".

--- api/src/Menor/Entity.js
+++ api/src/Menor/Entity.js
@@ -3,147 +3,147 @@
 import Joi from "Joi";
 
 export default class Entity {
-	constructor(deps = {}) {
-		this.Adapter = deps.Adapter || require("./Adapter").default;
-	}
+    constructor(deps = {}) {
+        this.Adapter = deps.Adapter || require("./Adapter").default;
+    }
 
-	create(body) {
-		const adapter = new this.Adapter();
-		return adapter.save(body);
-	}
+    create(body) {
+        const adapter = new this.Adapter();
+        return adapter.save(body);
+    }
 
-	fetchAll() {
-		const adapter = new this.Adapter();
-		return adapter.fetchAll();
-	}
+    fetchAll() {
+        const adapter = new this.Adapter();
+        return adapter.fetchAll();
+    }
 
-	fetchById(id) {
-		const adapter = new this.Adapter();
-		return adapter.fetchById(id);
-	}
+    fetchById(id) {
+        const adapter = new this.Adapter();
+        return adapter.fetchById(id);
+    }
 
-	find(body) {
-		const adapter = new this.Adapter();
-		return adapter.fetch(body.id);
-	}
+    find(body) {
+        const adapter = new this.Adapter();
+        return adapter.fetch(body.id);
+    }
 
-	remove(body) {
-		const adapter = new this.Adapter();
-		return adapter.delete(body.id);
-	}
+    remove(body) {
+        const adapter = new this.Adapter();
+        return adapter.delete(body.id);
+    }
 
-	update(body) {
-		const adapter = new this.Adapter();
-		return adapter.fetchAndUpdate(body);
-	}
+    update(body) {
+        const adapter = new this.Adapter();
+        return adapter.fetchAndUpdate(body);
+    }
 
-	getOrdination(body) {
-		const adapter = new this.Adapter();
-		return adapter.fetchOrdination();
-	}
+    getOrdination(body) {
+        const adapter = new this.Adapter();
+        return adapter.fetchOrdination();
+    }
 
-	addIntersting(body) {
-		const adapter = new this.Adapter();
-		return adapter.addIntersting();
-	}
+    addIntersting(body) {
+        const adapter = new this.Adapter();
+        return adapter.addIntersting();
+    }
 
-	fetchAllIntersting(body) {
-		const adapter = new this.Adapter();
-		return adapter.fetchAllInstersting();
-	}
+    fetchAllIntersting(body) {
+        const adapter = new this.Adapter();
+        return adapter.fetchAllInstersting();
+    }
 
-	removeIntersting(body) {
-		const adapter = new this.Adapter();
-		return adapter.removeIntersting();
-	}
+    removeIntersting(body) {
+        const adapter = new this.Adapter();
+        return adapter.removeIntersting();
+    }
 
-	createImage(body) {
-		const adapter = new this.Adapter();
-		return adapter.createImage();
-	}
+    createImage(body) {
+        const adapter = new this.Adapter();
+        return adapter.createImage();
+    }
 
-	fetchImages(body) {
-		const adapter = new this.Adapter();
-		return adapter.fetchAllImage();
-	}
+    fetchImages(body) {
+        const adapter = new this.Adapter();
+        return adapter.fetchAllImage();
+    }
 
-	fetchImage(body) {
-		const adapter = new this.Adapter();
-		return adapter.fetchImage();
-	}
+    fetchImage(body) {
+        const adapter = new this.Adapter();
+        return adapter.fetchImage();
+    }
 
-	removeImage(body) {
-		const adapter = new this.Adapter();
-		return adapter.removeImage();
-	}
+    removeImage(body) {
+        const adapter = new this.Adapter();
+        return adapter.removeImage();
+    }
 
-	createVideo(body) {
-		const adapter = new this.Adapter();
-		return adapter.createVideo();
-	}
+    createVideo(body) {
+        const adapter = new this.Adapter();
+        return adapter.createVideo();
+    }
 
-	fetchAllVideo(body) {
-		const adapter = new this.Adapter();
-		return adapter.fetchAllVideo();
-	}
+    fetchAllVideo(body) {
+        const adapter = new this.Adapter();
+        return adapter.fetchAllVideo();
+    }
 
-	fetchVideo(body) {
-		const adapter = new this.Adapter();
-		return adapter.fetchVideo();
-	}
+    fetchVideo(body) {
+        const adapter = new this.Adapter();
+        return adapter.fetchVideo();
+    }
 
-	removeVideo(body) {
-		const adapter = new this.Adapter();
-		return adapter.removeVideo();
-	}
+    removeVideo(body) {
+        const adapter = new this.Adapter();
+        return adapter.removeVideo();
+    }
 
-	validateToken(body) {
-		return new Promise((resolve, reject) => {
-			resolve(body);
-		});
-	}
+    validateToken(body) {
+        return new Promise((resolve, reject) => {
+            resolve(body);
+        });
+    }
 
-	validate(body) {
-		const schema = Joi.object({
-			nome: Joi.string().required(),
-			sexo: Joi.string().required().regex(/M|F/),
-			dataNascimento: Joi.date().required().max("now").min("now"), //somar data atual + 18 anos
-			refEtnia: Joi.object().required(),
-			certidaoNascimento: Joi.string().required(),
-			familyReferences: Joi.object(),
-			menoresVinculados: Joi.object().required(),
-			adocoesConjuntas: Joi.object().required(),
-			saudavel: Joi.boolean().required(),
-			descricaoSaude: Joi.string().required(),
-			curavel: Joi.boolean().required(),
-			deficienciaFisica: Joi.boolean().required(),
-			deficienciaMental: Joi.boolean().required(),
-			guiaAcolhimento: Joi.string().required(),
-			refCidade: Joi.string().required(),
-			refAbrigo: Joi.string().required(),
-			processoPoderFamiliar: Joi.string().required(),
-			interesses: Joi.string().required(),
-			visualizacoes: Joi.string().required(),
-			ativo: Joi.boolean().required()
-		});
+    validate(body) {
+        const schema = Joi.object({
+            nome: Joi.string().required(),
+            sexo: Joi.string().required().regex(/M|F/),
+            dataNascimento: Joi.date().required().max("now").min(new Date().setFullYear(new Date().getFullYear() - 18)), //somar data atual + 18 anos
+            Etnia: Joi.string().required(),
+            certidaoNascimento: Joi.string().required(),
+            familyReferences: Joi.object(),
+            menoresVinculados: Joi.object().required(),
+            adocoesConjuntas: Joi.object().required(),
+            saudavel: Joi.boolean().required(),
+            descricaoSaude: Joi.string().required(),
+            curavel: Joi.boolean().required(),
+            deficienciaFisica: Joi.boolean().required(),
+            deficienciaMental: Joi.boolean().required(),
+            guiaAcolhimento: Joi.string().required(),
+            refCidade: Joi.string().required(),
+            refAbrigo: Joi.string().required(),
+            processoPoderFamiliar: Joi.string().required(),
+            interesses: Joi.string().required(),
+            visualizacoes: Joi.string().required(),
+            ativo: Joi.boolean().required()
+        });
 
-		const {
-			error,
-			value
-		} = Joi.validate(body, schema);
+        const {
+            error,
+            value
+        } = Joi.validate(body, schema);
 
-		return new Promise((resolve, reject) => {
-			if (error) {
-				let messages = error.details.map(e => e.message);
-				reject({
-					status: 400,
-					messages
-				});
-			} else if (value) {
-				resolve(value);
-			}
-		});
+        return new Promise((resolve, reject) => {
+            if (error) {
+                let messages = error.details.map(e => e.message);
+                reject({
+                    status: 400,
+                    messages
+                });
+            } else if (value) {
+                resolve(value);
+            }
+        });
 
-	}
+    }
 
 }
--- api/src/Model/menor.js
+++ api/src/Model/menor.js
@@ -9,82 +9,85 @@ const processoSchema = require("./processo.js");
 const midiaSchema = require("./midia.js");
 
 const menorSchema = new Schema({
-	nome: {
-		type: String,
-		required: true
-	},
-	sexo: {
-		type: String,
-		enum: ["Feminino", "Masculino"],
-		required: true
-	},
-	certidaoNascimento: {
-		type: String
-	},
-	dataNascimento: {
-		type: Date,
-		required: true
-	},
-	refEtnia: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "etnias"
-	},
-	familiares: [
-		familiarSchema
-	],
-	menoresVinculados: [
-		vinculoSchema
-	],
-	adocoesConjuntas: [{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "menores"
-	}],
-	saudavel: {
-		type: Boolean,
-		required: true
-	},
-	descricaoSaude: {
-		type: String
-	},
-	curavel: {
-		type: Boolean
-	},
-	deficienciaFisica: {
-		type: Boolean
-	},
-	deficienciaMental: {
-		type: Boolean
-	},
-	guiaAcolhimento: {
-		type: String
-	},
-	refCidade: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "cidades"
-	},
-	refAbrigo: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "abrigos"
-	},
-	processoPoderFamiliar: {
-		type: processoSchema
-	},
-	interesses: [{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "interesses"
-	}],
-	midias: [
-		midiaSchema
-	],
-	visualizacoes: [{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "visualizacoes"
-	}],
-	ativo: {
-		type: Boolean,
-		required: true,
-		default: true
-	}
+    nome: {
+        type: String,
+        required: true
+    },
+    sexo: {
+        type: String,
+        enum: ["Feminino", "Masculino"],
+        required: true
+    },
+    certidaoNascimento: {
+        type: String
+    },
+    dataNascimento: {
+        type: Date,
+        required: true
+    },
+    Etnia: {
+        //type: mongoose.Schema.Types.ObjectId,
+        //ref: "etnias"
+        type: String,
+        enum: ["Branca", "Negra", "Parda", "Indígena", "Amarela", "Outras"],
+        required: true
+    },
+    familiares: [
+        familiarSchema
+    ],
+    menoresVinculados: [
+        vinculoSchema
+    ],
+    adocoesConjuntas: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "menores"
+    }],
+    saudavel: {
+        type: Boolean,
+        required: true
+    },
+    descricaoSaude: {
+        type: String
+    },
+    curavel: {
+        type: Boolean
+    },
+    deficienciaFisica: {
+        type: Boolean
+    },
+    deficienciaMental: {
+        type: Boolean
+    },
+    guiaAcolhimento: {
+        type: String
+    },
+    refCidade: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "cidades"
+    },
+    refAbrigo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "abrigos"
+    },
+    processoPoderFamiliar: {
+        type: processoSchema
+    },
+    interesses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "interesses"
+    }],
+    midias: [
+        midiaSchema
+    ],
+    visualizacoes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "visualizacoes"
+    }],
+    ativo: {
+        type: Boolean,
+        required: true,
+        default: true
+    }
 });
 
 mongoose.model("Menor", menorSchema);
