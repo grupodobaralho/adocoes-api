384c129e322f00894fd35fffbcef799a641a12bc
Mateus Haas <mateus@mhaas.com.br>
2017-10-20 20:30:36 -0200

    #96 Adicionar função genérica para controle de aggregate do moongose.

--- api/src/Common/MoongoseHelper.js
+++ api/src/Common/MoongoseHelper.js
@@ -4,7 +4,7 @@ export default class MoongoseHelper {
      * @param schema MoongoseSchema | Moongose schema
      * @param pipeline Array | Lista de pipelines para utilizar no `aggregate`
      */
-    static aggregate(schema, pipeline) {
+    static aggregateMany(schema, pipeline) {
         return new Promise((resolve, reject) => {
             let data = [];
 
@@ -19,5 +19,38 @@ export default class MoongoseHelper {
                     resolve(data) 
                 });
         });
+    }
+
+    /*
+     * @param schema MoongoseSchema | Moongose schema
+     * @param pipeline Array | Lista de pipelines para utilizar no `aggregate`
+     */
+    static aggregateOne(schema, pipeline) {
+        return new Promise((resolve, reject) => {
+            let data = null;
+
+            schema
+                .aggregate(pipeline)
+                .cursor()
+                .exec()
+                .on("data", (doc) => {
+                    data = doc;
+                })
+                .on("end", () => {
+                    resolve(data)
+                });
+        });
+    }
+
+    /*
+     * @param schema MoongoseSchema | Moongose schema
+     * @param pipeline Array | Lista de pipelines para utilizar no `aggregate`
+     */
+    static aggregate(schema, pipeline, isSingleRecord = false) {
+        let aggregateFunction = isSingleRecord ?
+            this.aggregateOne :
+            this.aggregateMany;
+
+        return aggregateFunction(schema, pipeline);
     }
 }
