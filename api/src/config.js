b74dee667b2c15ddee13febc37f0b76c00761b41
Mateus Haas <mateus@mhaas.com.br>
2017-10-26 22:22:07 -0200

    #101 Reapontar mongodb de HML para MLAB.

--- api/src/config.js
+++ api/src/config.js
@@ -8,7 +8,7 @@ export const config = {
     databaseUri: "mongodb://localhost:27017/AdocoesTest"
   },
   "homo": {
-    databaseUri: "mongodb://localhost:27017/Adocoes"
+    databaseUri: "mongodb://ages-adocoes:ages-adocoes@ds235775.mlab.com:35775/ages-adocoes"
   },
   "prod": {
     databaseUri: "mongodb://administrador:adocoesages2017@123@ds119302.mlab.com:19302/adocoes"
