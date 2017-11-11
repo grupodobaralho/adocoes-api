e6cf9ad5fc297b60c5e0cbe4614550e97eed3933
Mateus Haas <mateus@mhaas.com.br>
2017-11-11 13:37:01 -0200

    Arrumar parse de bytes do chunk.

--- api/src/Conteudo/Translator.js
+++ api/src/Conteudo/Translator.js
@@ -131,20 +131,20 @@ export default class Translator {
 
             const { range } = req.headers;
             const { size } = stats;
-            const start = Number((range || '').replace(/bytes/, '').split('-')[0]);
+            const start = Number((range || '').replace(/bytes=/, '').split('-')[0]);
             const end = size - 1;
             const chunkSize = (end - start) + 1;
 
             res.set({
-                'Content-Range': `bytes ${start}-${end}/${size}`,
+                "Content-Range": "bytes " + start + "-" + end + "/" + size,
                 'Accept-Ranges': 'bytes',
-                'Content-Length': chunkSize,
+                'Content-Length': size,
                 'Content-Type': 'video/mp4'
             });
 
             res.status(206);
 
-            const stream = fs.createReadStream(movieFile, { start, end });
+            const stream = fs.createReadStream(movieFile, {"start": start, "end": end, "autoClose": true});
 
             stream.on('open', () => stream.pipe(res));
             stream.on('error', (streamErr) => res.end(streamErr));
